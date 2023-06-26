4b3dbbed8af00914896cecb96f31e52d59a7faeb
Song Lin <song.lin@outlook.de>
2023-06-26 17:45:41 +0200

    cli app is using click

--- src/wikimlpcli.py
+++ src/wikimlpcli.py
@@ -5,12 +5,21 @@ from wikinlplib import summarize_wiki
 import click
 
 
-def phrase():
-    #fire.Fire(get_phrase)
+@click.command()
+@click.option('-f', '--func', required=True, type=click.Choice(['phrase','summary'], case_sensitive=False), help='Available functions, e.g. phrase, summary')
+@click.option('-n', '--name', required=True, help='Text for seraching')
+def cli(func, name):
+    """
+    cli application for wikipedia text
+    e.g., wikimlp --func=phrase --name=Germany
+    """
 
-def summary():
-    #fire.Fire(summarize_wiki)
+    if func == 'phrase' or func == 'PHRASE':
+        click.echo(get_phrase(name))
+    else:
+        click.echo(summarize_wiki(name))
 
-# if __name__ == '__main__':
-#    phrase()
+
+if __name__=='__main__':
+    cli()
     
